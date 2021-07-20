3690f8ef484cc708eefcaacecfd6d3bfcd2dfef8
mars444 <petromars444@gmail.com>
2021-07-20 23:19:15 +0300

    added works slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,16 @@
 $(function () {
 
+    $('.works__slider').slick({
+        nextArrow: '<button type="button" class="slick_button slick-next fa fa-chevron-right"></button>',
+        prevArrow: '<button type="button" class="slick_button slick-prev  fa fa-chevron-left"></button>',
+       slidesToShow: 3,
+       infinite:false,
+    
+    
+      });
+
+
+
+
 
 });
